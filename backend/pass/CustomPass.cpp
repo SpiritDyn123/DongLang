9fdedc9cbdf00847e8079384efd1b22f444cd178
dongyanan <dongyanan@gg.com>
2024-08-29 12:04:53 +0800

    pass 注册

--- backend/pass/CustomPass.cpp
+++ backend/pass/CustomPass.cpp
@@ -1,6 +1,8 @@
 #include "CustomPass.h"
-
+#include <iostream>
 using namespace llvm;
+using namespace std;
+
 PassBuilder PB;
 FunctionAnalysisManager FAM;
 FunctionPassManager FPM;
@@ -13,19 +15,21 @@ void InitCustomPass(legacy::PassManager* passMgr, legacy::FunctionPassManager*& 
 #if defined(CUSTOM_PASS_OPR) && CUSTOM_PASS_OPR != 0
 #if CUSTOM_PASS_OPR == 1
 	funPassMgr = new legacy::FunctionPassManager(&lm);
-	#define CUSTOM_PASS(passClass) addLe##passClass##Pass(funPassMgr);
+//add pass
+#define CUSTOM_PASS(passClass) addLe##passClass##Pass(funPassMgr);
+#include "backend/pass/PassConfig.def"
 #elif CUSTOM_PASS_OPR == 2
 	PB.registerLoopAnalyses(LAM);
 	PB.registerCGSCCAnalyses(CGAM);
 	PB.registerModuleAnalyses(MAM);
 	PB.registerFunctionAnalyses(FAM);
 	PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
-	#define CUSTOM_PASS(passClass) add##passClass##Pass(FPM);
+//add pass
+#define CUSTOM_PASS(passClass) add##passClass##Pass(FPM);
+#include "backend/pass/PassConfig.def"
 
 	//function pass ===> module pass
 	MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
 #endif
-	//add pass
-	#include "backend/pass/PassConfig.def"
 #endif
 }
